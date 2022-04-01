5afcb02265ea656428c526a05dcbb53d6d1d414c
kevin <kivito2009@hotmail.com>
2022-04-01 16:49:06 -0500

    config pero tienen errores

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-# AppDesktop
-Una aplicacion de escritorio realizada con electron y angular
-
-instalar los paquetes:
-- npm install --save-dev electron
-- npm i --save-dev electron-packager
-- npm i --save-dev @types/electron
-
-
-añadir un script en el package.json
-
-```"electron": "ng build && electron ."```
-
-ademas añadir tambien
-``` "main": "main.js", ```
-
-crear un file main.js a la altura de package.json
- con el contenido
-
-``` 
 const { app, BrowserWindow } = require("electron");
 
 let appWin;
@@ -52,11 +32,3 @@ app.on("window-all-closed", () => {
     app.quit();
   }
 });
- ```
-
-
-cambiar en el index.html
-```<base href="/">``` por ```<base href="./">```
-
-en el file angular.json cambiar
-``` "outputPath": "dist/app-desktop", ``` por ```"outputPath": "dist",```
